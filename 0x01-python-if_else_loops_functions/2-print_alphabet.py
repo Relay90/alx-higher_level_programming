1aafe395359a89ba86efe18b17bf8bb0ecbc2f59
Relay90 <motaungr511@gmail.com>
2023-11-28 03:31:56 -0800

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
-# Loop through the ASCII values for lowercase letters (97 to 122)
-for char in range(97, 123):
-    print("{}".format(chr(char)), end='')
-
-# Print a newline at the end (to match the expected behavior)
-print()
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")i
