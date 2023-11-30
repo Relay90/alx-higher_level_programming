802916409e253dbcbab07fea4f060e66e42ac472
Relay90 <motaungr511@gmail.com>
2023-11-30 10:04:31 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,13 +3,9 @@
 if __name__ == "__main__":
 
 from calculator_1 import add, sub, mul, div
-    a = 10
-    b = 5
 
-add_result = add(a, b)
-sub_result = sub(a, b)
-mul_result = mul(a, b)
-div_result = div(a, b)
+a = 10
+b = 5
 
 # Print the results
 print(f"{a} + {b} = {add_result}")
