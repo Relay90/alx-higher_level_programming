10f5adaa3dce3876baf7752aea5dc8c2b94f6fd0
Relay90 <motaungr511@gmail.com>
2023-11-30 10:32:58 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-import sys
 
-args = sys.argv[1:]
+    import sys
 
-num_args = len(args)
-
-if num_args == 0:
-    print("0 arguments.")
-elif num_args == 1:
-    print("1 argument:")
-    print("1: {}".format(args[0]))
-else:
-    print(f"{num_args} arguments:")
-    for i, arg in enumerate(args, start=1):
-        print(f"{i}: {arg}")i
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-import dis
-
-def print_hidden_names():
-    with open('hidden_4.pyc', 'rb') as file:
-        code = file.read()
-
-    # Disassemble the bytecode
-    instructions = dis.get_instructions(code)
-
-    # Extract names that don't start with '__'
-    names = {i.argval for i in instructions if isinstance(i.argval, str) and not i.argval.startswith('__')}
-    
-    # Print names in alphabetical order
-    for name in sorted(names):
-        print(name)
-
 if __name__ == "__main__":
-    print_hidden_names()
+
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
