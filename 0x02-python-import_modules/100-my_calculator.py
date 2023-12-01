96a61ecba8717e0a6b4859822c6fcc3d27d8a071
Relay90 <motaungr511@gmail.com>
2023-12-01 00:15:20 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
 
 if __name__ == "__main__":
+    import sys
+
     if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./calculator_1.py <a> <operator> <b>")
         sys.exit(1)
 
     a = int(sys.argv[1])
@@ -25,4 +37,4 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    print(f"{a} {operator} {b} = {result}")  # Corrected indentation to display the result
+    print(f"{a} {operator} {b} = {result}")
