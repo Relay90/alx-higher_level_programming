eb7f3011321decf12b953cc1122a2061556f96f5
Relay90 <motaungr511@gmail.com>
2023-12-03 20:49:02 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,5 @@
 def replace_in_list(my_list, idx, element):
     """Replace an element in a list at a specific position."""
     if idx >= 0 and idx < len(my_list):
-        return (my_lis)t
-    else:
         my_list[idx] = element
         return (my_list)
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    """Print all integers of a list in reverse order."""
+def print_reversed_list_integer(my_list):
+    """Prints integers of a list in reverse order."""
+    if not isinstance(my_list, list):
+        print("Input is not a list.")
+        return
+
     for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+        print(my_list[i])
