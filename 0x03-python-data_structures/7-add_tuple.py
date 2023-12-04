841516a921bd36582bbc9611de1f7dbf036d62a0
Relay90 <motaungr511@gmail.com>
2023-12-03 21:03:35 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    """
-    Add elements of two tuples and return a new tuple.
-
-    Args:
-    - tuple_a (tuple): First tuple (default is an empty tuple)
-    - tuple_b (tuple): Second tuple (default is an empty tuple)
-
-    Returns:
-    - tuple: A new tuple containing the element-wise sum of the input tuples
-    """
-
-    # Ensure tuple_a has at least two elements by padding with zeros if necessary
     if len(tuple_a) < 2:
         if len(tuple_a) == 0:  # If tuple_a is empty
             tuple_a = 0, 0
