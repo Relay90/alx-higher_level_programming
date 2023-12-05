d8c2cb4b717038f30bc424733cb950e075aa8498
Relay90 <motaungr511@gmail.com>
2023-12-04 23:19:33 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,17 +2,18 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2:
-        if len(tuple_a) == 0:  # If tuple_a is empty
+        if len(tuple_a) == 0:
             tuple_a = 0, 0
-        else:  # If tuple_a has only one element
+        else:
             tuple_a = tuple_a[0], 0
 
-    # Ensure tuple_b has at least two elements by padding with zeros if necessary
     if len(tuple_b) < 2:
-        if len(tuple_b) == 0:  # If tuple_b is empty
+        if len(tuple_b) == 0:
             tuple_b = 0, 0
-        else:  # If tuple_b has only one element
+        else:
             tuple_b = tuple_b[0], 0
 
-    # Calculate the sum of elements at corresponding indices and return a new tuple
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+            return (
+        tuple_a[0] + tuple_b[0],
+        tuple_a[1] + tuple_b[1]
+    )
