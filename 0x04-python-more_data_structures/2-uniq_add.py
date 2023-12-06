118530a305d8dda30a73063816581f132cc5b302
Relay90 <motaungr511@gmail.com>
2023-12-06 01:51:35 -0800

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_numbers = set()
+    sum_unique = 0
+    
+    for num in my_list:
+        if num not in unique_numbers:
+            sum_unique += num
+            unique_numbers.add(num)
+    return sum_unique
