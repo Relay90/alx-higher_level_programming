8eb3fa1c3c8c6bc052d807e1d274db03b024203a
Relay90 <motaungr511@gmail.com>
2023-12-18 10:46:02 -0800

    0x05-python-exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     try:
         result = fct(*args)
-    except Exception as e:
-        print("Exception: {}", format(e), file=sys.stderr)
-    finally:
         return result
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        return None
--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -6,49 +6,50 @@
 #include <bytesobject.h>
 #include <floatobject.h>
 #include <string.h>
-
 /**
- * print_python_bytes - Prints information about a Python bytes object
- * @p: Pointer to a Python object
+ * print_python_bytes - print python things
+ * @p: pointer to PyObject p
  */
 void print_python_bytes(PyObject *p)
 {
-	int i;
-	char *byte_data = PyBytes_AsString(p);
+	size_t b, i;
+	char *str;
 
-	Py_ssize_t size = PyBytes_Size(p);
-
-	if (p == NULL)
+	setbuf(stdout, NULL);
+	printf("[.] bytes object info\n");
+	if (PyBytes_Check(p) == 0)
 	{
-		printf("Error: Null pointer provided.\n");
+		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
-
-	if (!PyBytes_Check(p))
-	{
-		printf("Error: Object is not a bytes object.\n");
-		return;
-
-		printf("Total size of the bytes object: %zd\n", size);
-
-		printf("Byte data as a string (attempt):\n");
-		if (byte_data != NULL)
-		{
-			printf("%.*s\n", (int)size, byte_data);
-		}
-		else
-		{
-	printf("Error: Byte data is not valid ASCII or UTF-8 string.\n");
-		}
-		printf("First few bytes in hexadecimal format:\n");
-
-		for (i = 0; i < size && i < 10; ++i)
-		{
-			printf("%02x ", (unsigned char)byte_data[i]);
-		}
-		printf("\n");
+	str = ((PyBytesObject *)(p))->ob_sval, b = PyBytes_Size(p);
+	printf("  size: %ld\n  trying string: %s\n", b, str);
+	b >= 10 ? b = 10 : b++;
+	printf("  first %ld bytes: ", b);
+	for (i = 0; i < b - 1; i++)
+		printf("%02hhx ", str[i]);
+	printf("%02hhx\n", str[i]);
 }
+/**
+ * print_python_float - print python things
+ * @p: pointer to PyObject p
+ */
+void print_python_float(PyObject *p)
+{
+	char *str;
+	double f;
 
+	setbuf(stdout, NULL);
+	printf("[.] float object info\n");
+	if (PyFloat_Check(p) == 0)
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		return;
+	}
+	f = ((PyFloatObject *)(p))->ob_fval;
+	str = PyOS_double_to_string(f, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
+	printf("  value: %s\n", str);
+}
 /**
  * print_python_list - print python things
  * @p: pointer to PyObject p
@@ -77,25 +78,4 @@ void print_python_list(PyObject *p)
 		!strcmp(t, "bytes") ? print_python_bytes(list->ob_item[i]) : (void)t;
 		!strcmp(t, "float") ? print_python_float(list->ob_item[i]) : (void)t;
 	}
-}
-
-/**
- * print_python_float - print python things
- * @p: pointer to PyObject p
- */
-void print_python_float(PyObject *p)
-{
-	char *str;
-	double f;
-
-	setbuf(stdout, NULL);
-	printf("[.] float object info\n");
-	if (PyFloat_Check(p) == 0)
-	{
-		printf("  [ERROR] Invalid Float Object\n");
-		return;
-	}
-	f = ((PyFloatObject *)(p))->ob_fval;
-	str = PyOS_double_to_string(f, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
-	printf("  value: %s\n", str);
 }
