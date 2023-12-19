10d59bc541e76d5f18eaad9e4a44d2fdc40bb01d
Relay90 <motaungr511@gmail.com>
2023-12-18 20:59:42 -0800

    0x05-python-exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,11 +8,10 @@ def safe_print_list_integers(my_list=[], x=0):
                 if isinstance(my_list[i], int):
                     print("{:d}".format(my_list[i]), end="")
                     printed += 1
-            except (ValueError: IndexError):
-                raise IndexError
+            except ValueError:
+                pass
     except IndexError:
+        pass
+    finally:
         print("")
-        raise
-
-    print("")
-    return printed
+        return printed
