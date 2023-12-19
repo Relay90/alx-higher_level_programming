5215724772488828f156eff2da2ddee7cbbf953b
Relay90 <motaungr511@gmail.com>
2023-12-18 21:03:38 -0800

    0x05-python-exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 if isinstance(my_list[i], int):
                     print("{:d}".format(my_list[i]), end="")
                     printed += 1
-            except ValueError:
+            except (ValueError, IndexError):
                 pass
     except IndexError:
         pass
