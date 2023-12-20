d101781d6542f2c7a788c20d69f99a9b9d4103ae
Relay90 <motaungr511@gmail.com>
2023-12-19 21:00:01 -0800

    alx-higher_level_programming

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,8 +4,24 @@
 
 
 class Square:
-    """Class Square that defines a square."""
+    """
+    Class Square that defines a square.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+
     def __init__(self, size=0):
+        """
+        Initializes a square instance with a given size.
+
+        Args:
+            size (int): The size of the square (default is 0).
+        
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
 
         if type(size) != int:
             raise TypeError("size must be an integer")
