e04c4e4d16d8f8eb6b0fa7e9fddaad10e7e926b4
Relay90 <motaungr511@gmail.com>
2024-01-04 22:43:19 -0800

    0x09-python-everything_is_object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+
+
+class LockedClass:
+    def __setattr__(self, name, value):
+        if name != 'first_name':
+            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
+        super().__setattr__(name, value)
+
+lc = LockedClass()
+lc.first_name = "John"
+try:
+    lc.last_name = "Snow"
+except AttributeError as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
