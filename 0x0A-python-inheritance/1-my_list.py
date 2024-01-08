3a4bb3414ab5eda17f845e343b08a3ff0e94eb5b
Relay90 <motaungr511@gmail.com>
2024-01-08 10:32:33 -0800

    0x0A-python-inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,10 +7,10 @@ Module with class MyList
 
 
 class MyList(list):
+    """Class with method print_sorted"""
     pass
 
-
     def print_sorted(self):
-        """Prints the list in ascending order"""
+        """Methot that sorted a list"""
 
         print(sorted(list(self)))
