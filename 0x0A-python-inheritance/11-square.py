9de05f7dfd69d45dff6d145a485d8f3350d8b8ac
Relay90 <motaungr511@gmail.com>
2024-01-08 11:17:48 -0800

    0x0A-python-inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
 """
 ===================================
 module with class BaseGeometry
@@ -6,40 +7,20 @@ module with class BaseGeometry
 """
 
 
-
-class BaseGeometry:
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    def __init__(self, width, height):
-        self.__width = 0
-        self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
-
-
 class Square(Rectangle):
+    """Square class that inherits from Rectangle that inherits BaseGeometry"""
+
     def __init__(self, size):
-        self.__size = 0
+        """Method for initialized the attrubutes"""
+
+        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
+
+    def area(self):
+        """rectangle area"""
+
+        return self.__size ** 2
 
     def __str__(self):
-        return f"[Square] {self.__size}/{self.__size}"
+        return "[Square] {}/{}".format(self.__size, self.__size)
