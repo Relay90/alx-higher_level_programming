f09371b5f7f74e4ccb36a1fc87456a0d3607f497
Relay90 <motaungr511@gmail.com>
2024-01-09 11:50:21 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,26 +1,45 @@
 #!/usr/bin/python3
+"""Module defining the class Student based on 9-student.py"""
 
-import json
 
 class Student:
-    # ... (previous class definition remains the same)
+    """
+    Class that defines properties of student.
+
+    Attributes:
+        first_name (str): first name of student.
+        last_name (int): last name of student.
+        age (int): age of student.
+    """
+    def __init__(self, first_name, last_name, age):
+        """Creates new instances of Student.
+
+        Args:
+            first_name (str): first name of student.
+            last_name (int): last name of student.
+            age (int): age of student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
         """Retrieves a dictionary representation of a Student instance.
 
-        If attrs is a list of strings, only attribute names contained in
-        this list must be retrieved. Otherwise, all attributes must be retrieved.
+        If attrs is a list of strings, only attribute names contained in,
+        this list must be retrieved.
+        Otherwise, all attributes must be retrieved.
 
         Returns:
             dict: dictionary representation.
         """
         if attrs is None:
             return self.__dict__
-        
+
         new_dict = {}
         for item in attrs:
-            if hasattr(self, item):
-                value = getattr(self, item)
-                if isinstance(value, (str, int, bool, list, dict)):
-                    new_dict[item] = value
+            try:
+                new_dict[item] = self.__dict__[item]
+            except Exception:
+                pass
         return new_dict
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
+"""Module defining the class Student"""
 
-import json
 
 class Student:
-    # ... (previous class definition remains the same)
+    """
+    Class that defines properties of student.
+
+    Attributes:
+        first_name (str): first name of student.
+        last_name (int): last name of student.
+        age (int): age of student.
+    """
+    def __init__(self, first_name, last_name, age):
+        """Creates new instances of Student.
+
+        Args:
+            first_name (str): first name of student.
+            last_name (int): last name of student.
+            age (int): age of student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """Retrieves a dictionary representation of a Student instance.
@@ -11,8 +29,4 @@ class Student:
         Returns:
             dict: dictionary representation.
         """
-        student_dict = self.__dict__.copy()
-        # For non-serializable attributes, convert to a serializable type
-        # Example: Converting a custom object to a string representation
-        # student_dict['custom_attribute'] = str(student_dict['custom_attribute'])
-        return student_dict
+        return self.__dict__
