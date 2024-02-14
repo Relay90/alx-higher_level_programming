93d14505f1b6df3e3418c71e41d344e6c9deea25
Relay90 <motaungr511@gmail.com>
2024-02-14 02:43:56 -0800

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,3 @@
--- Import the table dump into the hbtn_0c_0 database
-USE hbtn_0c_0;
-SOURCE /path/to/your/table_dump.sql;
-
--- Display the top 3 cities' temperatures during July and August ordered by temperature (descending)
+-- List top 3 cities
+-- Execute: cat 102-top_city.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
