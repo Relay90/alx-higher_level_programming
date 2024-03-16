576ff6dba5569c6d71eb1eaedb6f9bfd8ee7149f
Relay90 <motaungr511@gmail.com>
2024-03-15 21:51:48 -0700

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Deletes all State objects with a name containing the letter 'a' from the database hbtn_0e_6_usa"""
+"""Deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa"""
 
 import sys
 from sqlalchemy import create_engine
@@ -7,24 +7,26 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+    # Check if all three arguments are provided
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
+        print("Usage: {} username password database".format(sys.argv[0]))
         sys.exit(1)
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+    # Database connection
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, password, database),
+                           pool_pre_ping=True)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, db_name), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
+    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_to_delete = session.query(State).filter(State.name.ilike('%a%')).all()
-
-    for state in states_to_delete:
+    # Query to delete all State objects with a name containing the letter 'a'
+    states_with_a = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states_with_a:
         session.delete(state)
-
     session.commit()
+
+    # Close session
     session.close()
