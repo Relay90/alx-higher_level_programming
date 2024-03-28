81513eb2255256d1b618a80b89b64a6affbebb2a
Relay90 <motaungr511@gmail.com>
2024-03-28 12:50:00 -0700

    0x11-python-network_1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,17 +4,10 @@ Script that fetches https://intranet.hbtn.io/status
 """
 import requests
 
-def main():
-    url = "https://intranet.hbtn.io/status"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        print("Success! Connection established.")
-        print("Body response:")
-        print("\t- type:", type(response.text))
-        print("\t- content:", response.text)
-    else:
-        print(f"Error: Failed to fetch URL. Status code: {response.status_code}")
-
 if __name__ == '__main__':
-    main()
+    url = "https://intranet.hbtn.io/status"
+    r = requests.get(url)
+    text = r.text
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
